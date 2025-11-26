1481d6e4df446287cd70920d08eb5d4429d5af08
milagorecki <mila.gorecki@tuebingen.mpg.de>
2025-11-26 13:05:20 +0000

    Deploying to gh-pages from @ milagorecki/milagorecki.github.io@2c351c7c49adaa55a86913908a97f860092114a8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -424,21 +424,11 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
+      },{id: "news-our-work-amortized-bayesian-decision-making-for-simulation-based-models-got-accepted-at-tmlr",
+          title: 'Our work “Amortized Bayesian Decision Making for simulation-based models” got accepted at TMLR!...',
           description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-our-work-monoculture-or-multiplicity-which-is-it-got-accepted-at-neurips",
+          title: 'Our work “Monoculture or Multiplicity: Which is it?” got accepted at NeurIPS!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
@@ -486,46 +476,25 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
+        id: 'social-bluesky',
+        title: 'Bluesky',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("mailto:%6D%69%6C%61.%67%6F%72%65%63%6B%69@%74%75%65%62%69%6E%67%65%6E.%6D%70%67.%64%65", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=9_3s6VUAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
